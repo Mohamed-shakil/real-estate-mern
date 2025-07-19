6b434e154300db3c46a7cbc7a0dd4cefae53eff2
Mohamed-shakil <shakilmohammed16@gmail.com>
2025-07-19 12:38:19 +0530

    Route update and header addition

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      <p>hello</p>
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      
+    </Routes>
+    </BrowserRouter>
+
   )
 }
 
